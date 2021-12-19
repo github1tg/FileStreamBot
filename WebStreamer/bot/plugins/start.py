6a11bf6d4e1faadde548c5cde86ff2380f190742
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-19 07:07:02 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -9,7 +9,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
 db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
 
-START_TEXT = """
+START_PHOTO = """
+photo="https://i.ibb.co/NKXgXD4/vlmnwosn-0.png",
+START_CAPTION = 
 <i>Hᴇʏ 🙋,</i>{}\n
 <i>𝐼'𝑚 𝑡𝑒𝑙𝑒𝑔𝑟𝑎𝑚 𝑓𝑖𝑙𝑒𝑠 𝑠𝑡𝑟𝑒𝑎𝑚𝑖𝑛𝑔 𝑏𝑜𝑡 𝑎𝑠 𝑤𝑒𝑙𝑙 𝑑𝑖𝑟𝑒𝑐𝑡 𝑙𝑖𝑛𝑘𝑠 𝑔𝑒𝑛𝑒𝑟𝑎𝑡</i>\n
 <i>𝐶𝑙𝑖𝑐𝑘 𝑜𝑛 ℎ𝑒𝑙𝑝 𝑡𝑜 𝑔𝑒𝑡 𝑚𝑜𝑟𝑒 𝑖𝑛𝑓𝑜𝑟𝑚𝑎𝑡𝑖𝑜𝑛</i>\n
