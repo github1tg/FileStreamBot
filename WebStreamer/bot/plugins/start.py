ec1f81bf91c7c4d005959f5871b9949cd8756d2c
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-19 07:41:49 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -60,7 +60,7 @@ ABOUT_BUTTONS = InlineKeyboardMarkup(
 @StreamBot.on_callback_query()
 async def cb_data(bot, update):
     if update.data == "home":
-        await update.message.edit_text(
+        await update.message.edit_caption(
             caption=START_CAPTION.format(update.from_user.mention),
             disable_web_page_preview=True,
             reply_markup=START_BUTTONS
